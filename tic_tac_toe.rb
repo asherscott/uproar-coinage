7e1acd20453cdc3d3aec21e48d158b16bb9b0060
asher scott <asher.scott6@gmail.com>
2022-07-07 17:07:12 -0500

    refactored row checks for n x n

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -10,28 +10,11 @@ class TicTacToe
 
     # row checks
 
-    if row1[0] == "o" && row1[1] == "o" && row1[2] == "o"
-      return "o"
-    end
+    @board.each do |row|
+      # for each row of board, if all values are the same and not a blank ' ' => return that value
+      set = row.uniq
 
-    if row2[0] == "o" && row2[1] == "o" && row2[2] == "o"
-      return "o"
-    end
-
-    if row3[0] == "o" && row3[1] == "o" && row3[2] == "o"
-      return "o"
-    end
-
-    if row1[0] == "x" && row1[1] == "x" && row1[2] == "x"
-      return "x"
-    end
-
-    if row2[0] == "x" && row2[1] == "x" && row2[2] == "x"
-      return "x"
-    end
-
-    if row3[0] == "x" && row3[1] == "x" && row3[2] == "x"
-      return "x"
+      return set[0] if set.length == 1 && set[0] != " "
     end
 
     # column checks
