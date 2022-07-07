ef08e4c6b34386219cf538b23e2b1d450d41008a
asher scott <asher.scott6@gmail.com>
2022-07-07 18:38:58 -0500

    column checks refactor n x n

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -19,28 +19,15 @@ class TicTacToe
 
     # column checks
 
-    if row1[0] == "o" && row2[0] == "o" && row3[0] == "o"
-      return "o"
-    end
+    @board.each_with_index do |col, idx|
+      column_vals = []
 
-    if row1[1] == "o" && row2[1] == "o" && row3[1] == "o"
-      return "o"
-    end
+      @board.each do |row|
+        column_vals << row[idx]
+      end
 
-    if row1[2] == "o" && row2[2] == "o" && row3[2] == "o"
-      return "o"
-    end
-
-    if row1[0] == "x" && row2[0] == "x" && row3[0] == "x"
-      return "x"
-    end
-
-    if row1[1] == "x" && row2[1] == "x" && row3[1] == "x"
-      return "x"
-    end
-
-    if row1[2] == "x" && row2[2] == "x" && row3[2] == "x"
-      return "x"
+      set = column_vals.uniq
+      return set[0] if set.length == 1 && set[0] != " "
     end
 
     # diagonal checks
